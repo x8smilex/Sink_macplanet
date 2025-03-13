3f978e3eb5f39da577510488379a7d82eb3de454
x8smilex <88746316+x8smilex@users.noreply.github.com>
2025-03-13 15:01:39 +0700

    Update Footer.vue

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -1,8 +1,4 @@
-<script setup>
-import { BloggerIcon, GitHubIcon, GmailIcon, MastodonIcon, TelegramIcon, XIcon } from 'vue3-simple-icons'
 
-const { title, email, telegram, blog, twitter, mastodon, github } = useAppConfig()
-</script>
 
 <template>
   <section class="md:pt-6">
@@ -14,89 +10,15 @@ const { title, email, telegram, blog, twitter, mastodon, github } = useAppConfig
       >{{ title }}</a>
       <a
         class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
-        href="https://html.zone"
+        href="https://macplanet.vn"
         target="_blank"
-        title="HTML.ZONE"
+        title="Macplanet."
       >
-        &copy; {{ new Date().getFullYear() }} Products of HTML.ZONE
+        &copy; {{ new Date().getFullYear() }} Macplanet.
       </a>
       <span
         class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start"
-      >
-        <a
-          v-if="email"
-          :href="`mailto:${email}`"
-          title="Email"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Email</span>
-          <GmailIcon
-            class="w-6 h-6"
-          />
-        </a>
-        <a
-          v-if="telegram"
-          :href="telegram"
-          target="_blank"
-          title="Telegram"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Telegram</span>
-          <TelegramIcon
-            class="w-6 h-6"
-          />
-        </a>
-        <a
-          v-if="blog"
-          :href="blog"
-          target="_blank"
-          title="Blog"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Blog</span>
-          <BloggerIcon
-            class="w-6 h-6"
-          />
-        </a>
-
-        <a
-          v-if="twitter"
-          :href="twitter"
-          target="_blank"
-          title="Twitter"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Twitter</span>
-          <XIcon
-            class="w-6 h-6"
-          />
-        </a>
-
-        <a
-          v-if="mastodon"
-          :href="mastodon"
-          target="_blank"
-          title="Mastodon"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Mastodon</span>
-          <MastodonIcon
-            class="w-6 h-6"
-          />
-        </a>
-
-        <a
-          v-if="github"
-          :href="github"
-          target="_blank"
-          title="GitHub"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">GitHub</span>
-          <GitHubIcon
-            class="w-6 h-6"
-          />
-        </a>
+  
       </span>
     </div>
   </section>
