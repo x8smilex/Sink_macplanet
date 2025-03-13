f9b628d96ccfc9dd4124e9d3732307f120eb2287
x8smilex <88746316+x8smilex@users.noreply.github.com>
2025-03-13 15:03:02 +0700

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
   telegram: 'https://sink.cool/telegram',
   mastodon: 'https://sink.cool/mastodon',
   blog: 'https://sink.cool/blog',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  description: 'Rút gọn Link cho Macplanet.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
